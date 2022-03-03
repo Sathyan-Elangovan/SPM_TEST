a8c191da6130ab964735a7c0e833ea26af3f212f
Sathyan Elangovan <sathya576@gmail.com>
2022-03-03 11:54:28 +0530

    Initial Commit

---
+++ SPM MIGRATION/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SPM MIGRATION
+//
+//  Created by sathyan elangovan on 03/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
